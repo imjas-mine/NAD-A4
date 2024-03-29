7376c70cf4daadca413babd28f04dc26def8e2c0
imjas-mine <116243241+imjas-mine@users.noreply.github.com>
2024-03-29 17:30:47 -0400

    created post cards

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,34 +1,54 @@
-console.log("hello world!!!")
+console.log("hello world!!!");
 
-const helloWorldBox=document.getElementById("hello-world")
-const postsBox=document.getElementById("posts-box")
-
-
-$.ajax({
-    type:'GET',
-    url:'/hello world/',
-    success: function(response){
-        console.log('success',response.text)
-        helloWorldBox.textContent=response.text
-    },
-    error:function(error){
-        console.log('error',error)
-    }
-})
+const helloWorldBox = document.getElementById("hello-world");
+const postsBox = document.getElementById("posts-box");
+const spinnerBox = document.getElementById("spinner-box");
 
 $.ajax({
-    type:'GET',
-    url:'/data/',
-    success:function(response){
-        const data=response.data
-        console.log(data)
-        data.forEach(el => {
-            postsBox.innerHTML+=`
-            ${el.title}-<b>${el.body}</b><br>
-            `
-        });
-    },
-    error: function(error){
-        console.log(error)
-    }
-})
+  type: "GET",
+  url: "/hello world/",
+  success: function (response) {
+    console.log("success", response.text);
+    helloWorldBox.textContent = response.text;
+  },
+  error: function (error) {
+    console.log("error", error);
+  },
+});
+
+$.ajax({
+  type: "GET",
+  url: "/data/",
+  success: function (response) {
+    const data = response.data;
+    console.log(data);
+    setTimeout(() => {
+      spinnerBox.classList.add(".not-visible");
+      data.forEach((el) => {
+        postsBox.innerHTML += `
+        <div class="card mb-2"">
+         
+            <div class="card-body">
+                <h5 class="card-title">${el.title}</h5>
+                <p class="card-text">${el.body}</p>
+            </div>
+            <div class="card-footer">
+                <div class="row">
+                    <div class="col-1">
+                        <a href="#" class="btn btn-primary">Details</a>
+
+                    </div>
+                    <div class="col-1">
+                        <a href="#" class="btn btn-primary">Like</a>
+
+                    </div>
+                </div>
+             </div>
+                `
+      });
+    }, 100);
+  },
+  error: function (error) {
+    console.log(error);
+  },
+});
