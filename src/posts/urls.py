1f9b77241ca2aa252ba06f21bc34c25500332ca9
imjas-mine <116243241+imjas-mine@users.noreply.github.com>
2024-03-30 12:01:14 -0400

    created the button which would like and unlike posts using ajax

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -4,6 +4,7 @@ from .views import(
     post_list_and_create,
     hello_world_view,
     load_post_data_view,
+    like_unlike_post
 )
 
 app_name='posts'
@@ -11,4 +12,5 @@ urlpatterns=[
     path('',post_list_and_create,name='main-board'),
     path('data/<int:num_posts>/',load_post_data_view,name='posts-data'),
    path('hello world/',hello_world_view,name='hello-world'),
+   path('like-unlike/', like_unlike_post,name='like-unlike'),
 ]
