af82a696342313fbf59f90f518ae99dedf50c5c7
imjas-mine <116243241+imjas-mine@users.noreply.github.com>
2024-03-31 13:45:18 -0400

    created the post detail page

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -9,6 +9,7 @@ const title=document.getElementById('id_title')
 const body=document.getElementById('id_body')
 const csrf=document.getElementsByName('csrfmiddlewaretoken')
 const alertBox=document.getElementById('alert-box')
+const url=window.location.href
 
 console.log('csrf',csrf[0].value)
 
@@ -75,7 +76,7 @@ const getData=()=>{
                   <div class="card-footer">
                       <div class="row">
                           <div class="col-2">
-                              <a href="#" class="btn btn-primary">Details</a>
+                              <a href="${url}${el.id}" class="btn btn-primary">Details</a>
       
                           </div>
                           <div class="col-2">
