b7860ecfd8096d1ad954a3d95250c82e156b0012
imjas-mine <116243241+imjas-mine@users.noreply.github.com>
2024-04-01 01:10:09 -0400

    deleting posts with ajax and working with local storage

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -1,5 +1,6 @@
 console.log("heelo worlddetail ");
-
+const csrf=document.getElementsByName('csrfmiddlewaretoken')
+const alertBox=document.getElementById('alert-box')
 const postBox=document.getElementById('post-box')
 const backBtn=document.getElementById('back-btn')
 const updateBtn=document.getElementById('update-btn')
@@ -8,6 +9,12 @@ const url=window.location.href+"data/"
 const spinnerBox=document.getElementById('spinner-box')
 const titleInput=document.getElementById('id_title')
 const bodyInput=document.getElementById('id_body')
+const updateUrl=window.location.href+"update/"
+const deleteUrl=window.location.href+"delete/"
+
+
+const updateForm=document.getElementById('update-form')
+const deleteForm=document.getElementById('delete-form')
 
 
 
@@ -34,9 +41,11 @@ $.ajax({
 
         const titleEl=document.createElement('h3')
         titleEl.setAttribute('class','mt-3')
+        titleEl.setAttribute('id','title')
 
         const bodyEl=document.createElement('p')
         bodyEl.setAttribute('class','mt-1')
+        bodyEl.setAttribute('id','body')
 
         titleEl.textContent=data.title
         bodyEl.textContent=data.body
@@ -52,4 +61,51 @@ $.ajax({
     error:function(error){
         console.log(error)
     }
+})
+
+updateForm.addEventListener('submit',e=>{
+    e.preventDefault()
+
+    const title=document.getElementById('title')
+    const body=document.getElementById('body')
+
+    $.ajax({
+        type:'POST',
+        url:updateUrl,
+        data:{
+                'csrfmiddlewaretoken':csrf[0].value,
+                'title':titleInput.value,
+                'body':bodyInput.value,
+            },
+            success: function(response){
+                console.log(response)
+                handleAlerts('success','post has been updated')
+                title.textContent=response.title
+                body.textContent=response.body
+            },
+            error:function(error){
+                console.log(error)
+            }
+    })
+})
+
+deleteForm.addEventListener('submit',e=>{
+    e.preventDefault()
+ 
+
+    $.ajax({
+        type:'POST',
+        url:deleteUrl,
+        data:{
+                'csrfmiddlewaretoken':csrf[0].value,
+                
+            },
+            success: function(response){
+                window.location.href=window.location.origin
+                localStorage.setItem('title',titleInput.value)
+            },
+            error: function(error){
+                console.log(error)
+            }
+    })
 })
