d2db528ccc767d9a305415102f16e469ed0cc2ea
imjas-mine <116243241+imjas-mine@users.noreply.github.com>
2024-04-01 15:16:24 -0400

    created the user profile view

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -6,3 +6,17 @@ from django.http import JsonResponse
 def my_profile_view(request):
     obj=Profile.objects.get(user=request.user)
     form=ProfileForm(request.POST or None, request.FILES or None, instance=obj)
+    if request.method=='POST':
+        if form.is_valid():
+            instance=form.save()
+            return JsonResponse({
+                'bio':instance.bio,
+                'avatar':instance.avatar.url,
+                'user':instance.user.username
+            })
+    context={
+        'obj':obj,
+        'form':form,
+    }
+    
+    return render(request,'profiles/main.html',context)
